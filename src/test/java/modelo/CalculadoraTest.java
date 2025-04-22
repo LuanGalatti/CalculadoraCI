0b899e37ea09f0e4614a61e7ff830060c3c947db
LuanGalatti <luan.galatti@gmail.com>
2025-04-22 09:42:47 -0300

    Update: removido testes

--- src/test/java/modelo/CalculadoraTest.java
+++ src/test/java/modelo/CalculadoraTest.java
@@ -45,40 +45,6 @@ class CalculadoraTest {
         double expResult = 2.0;
         double result = instance.getDiferenca();
         assertEquals(expResult, result, 0);
-
-    }
-
-    /**
-     * Teste do método getProduto.
-     */
-    @Test
-    void testGetProduto() {
-        double expResult = 8.0;
-        double result = instance.getProduto();
-        assertEquals(expResult, result, 0);
-
-    }
-
-    /**
-     * Teste do método getQuociente.
-     */
-    @Test
-    void testGetQuociente() {
-        double expResult = 2.0;
-        double result = instance.getQuociente();
-        assertEquals(expResult, result, 0);
-
-    }
-
-    /**
-     * Teste do método getExponencial.
-     */
-    @Test
-    void testGetExponencial() {
-        double expResult = 16.0;
-        double result = instance.getExponencial();
-        assertEquals(expResult, result, 0);
-
     }
 
     @AfterAll
